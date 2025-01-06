24334b8ea94adc549f41a3bab9c6c8cbf0c1a7dc
dina570 <dinayulianingsih97@gmail.com>
2025-01-06 12:03:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+    <head>
+        <title> Documen </title>
+    </head>
+
+    <body>
+        <h3>
+            <?php
+            echo"Hello world"
+            ?>
+            </h3>
+</body>
+</html>
