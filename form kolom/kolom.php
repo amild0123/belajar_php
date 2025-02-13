7362ed81a0ebc06652cd83242cd98772d6e90faa
amild0123 <Amild384@gmail.com>
2025-02-13 11:15:15 +0700

    goo

--- form kolom/kolom.php
+++ form kolom/kolom.php
@@ -1,5 +1,51 @@
-<p> <label> Kolom pertama   :  </label>
-<input type="text" name="satu" placeholder="masukan nilai gaji anda">
+<!DOCTYPE HTML>
+<HTML lang="en">
+    <head>
+        <title> documen </title>
+    </head>
+
+
+<body>
+    <p> <label> Kolom   :  </label>
+<input type="text" name="satu" placeholder="masukan nilai untuk kolom">
 </p>
 
-<tabel border=1> 
+<p> <label> Baris   :  </label>
+<input type="text" name="dua" placeholder="masukan nilai untuk baris">
+</p>
+
+<tr>
+    <td colspan="3">
+        <input type="submit" value="submit" name="Run">
+    </td>
+</tr>
+
+<?php 
+
+if(isset($_POST['Run']))
+$baris= $_POST['baris'];
+$kolom= $_POST['kolom'];
+?>
+
+<table border=1 style="width: 75%;">
+
+<?php 
+for($i=1; $i<= $baris; $i++) {
+    ?>
+     <tr>
+ <?php
+     for($y = 1; $y <= $kolom; $y++) {
+   ?>
+    <td>
+      &nbsp;
+    </td>
+    <?php
+        }
+       ?>
+   </tr>
+                <?php
+}
+?>  
+</table>
+</body>
+    </html>
